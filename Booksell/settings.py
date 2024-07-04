b900a2fb00f0e06c92116accaab80b1e63109238
moon <78158092+SIFAT11@users.noreply.github.com>
2024-07-04 11:57:53 +0600

    Initial commit

--- Booksell/settings.py
+++ Booksell/settings.py
@@ -120,11 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+ 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
