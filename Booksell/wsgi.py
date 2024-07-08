ec0549d5077914f11db3002a52498c97607ab220
moon <78158092+SIFAT11@users.noreply.github.com>
2024-07-08 16:32:29 +0600

    Initial commit

--- Booksell/wsgi.py
+++ Booksell/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Booksell.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookShop.settings')
 application = get_wsgi_application()
